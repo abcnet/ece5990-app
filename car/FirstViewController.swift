fd8c98f6dde16957e017a4175e99a8f618ae60ad
abcnet <zr@macbook-pro-2.local>
2016-04-13 01:28:28 -0400

    Initial Commit

---
+++ car/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  car
+//
+//  Created by Zhiyun Ren on 4/13/16.
+//  Copyright © 2016 Zhiyun Ren. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
