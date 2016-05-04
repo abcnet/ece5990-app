c6069103130620ce0d07edf46781616305783450
abcnet <zr@dhcp-rhodes-1439.eduroam.cornell.edu>
2016-05-04 19:13:35 -0400

    ip and time

--- car/SecondViewController.swift
+++ car/SecondViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class SecondViewController: UIViewController {
+    
+    // Create a reference to a Firebase location
+    var myRootRef = Firebase(url:"https://ece5990.firebaseio.com/ip")
+    // Write data to Firebase
+   
 
+    @IBOutlet weak var secondViewText: UILabel!
+    @IBOutlet weak var secondViewSubtitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Create a reference to a Firebase location
+        // Read data and react to changes
+
+        myRootRef.observeEventType(.Value, withBlock: {
+            snapshot in
+//            self.secondViewSubtitle.text = 
+            let str = "\(snapshot.value)"
+            print(str)
+            let ipStart = str.rangeOfString("ip = \"")?.endIndex
+            let timeStart = str.rangeOfString("time = \"")?.endIndex
+
+            let ipsubstr = str.substringFromIndex(ipStart!)
+            let timesubstr = str.substringFromIndex(timeStart!)
+            let ip = ipsubstr.substringToIndex((ipsubstr.rangeOfString("\"")?.startIndex)!)
+            let timeStr = timesubstr.substringToIndex((timesubstr.rangeOfString("\"")?.startIndex)!)
+            self.secondViewText.text = ip
+            self.secondViewSubtitle.text = timeStr
+        })
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
